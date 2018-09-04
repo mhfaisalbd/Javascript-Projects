04f038c9f710bdb83e7125230e1adc8fb7c5ea82
mhfaisalbd <bdmhfaisal@gmail.com>
2018-09-04 23:14:42 +0600

    the project digital clock is done.

---
+++ Digital Clock/clock.js
@@ -0,0 +1,23 @@
+setInterval ( function() {
+	var currentTime = new Date();
+	var hours = currentTime.getHours();
+	var munites = currentTime.getMinutes();
+	var  seconds = currentTime.getSeconds();
+	var periods = "AM";
+	if (hours >= 12){
+		periods = "PM";
+	}
+	if (hours > 12){
+		hours = hours - 12;
+	}
+	if (munites < 10){
+		munites = "0" + munites;
+	}
+	if (seconds < 10){
+		seconds = "0" + seconds;
+	}
+		var clockTime = hours + ":" + munites + ":" + seconds + " " + periods;
+	
+	var clock = document.getElementById('clock');
+	clock.innerText = clockTime;
+}, 1000);
